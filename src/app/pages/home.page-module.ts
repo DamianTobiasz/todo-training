0e0d4b80da5321fc834c62d886fc2577506b09dc
Damian Tobiasz <damiant12345@gmail.com>
2022-03-03 11:38:32 +0100

    added dropdownbutton

--- src/app/pages/home.page-module.ts
+++ src/app/pages/home.page-module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
-@NgModule({ imports: [CommonModule,
-      RouterModule.forChild([
-        {
-          path: '',
-          component: HomePage,
-        }
-      ])],
-declarations: [HomePage],
-providers: [],
-exports: [] })
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      }
+    ])],
+  declarations: [HomePage],
+  providers: [],
+  exports: []
+})
 export class HomePageModule {
 }
--- src/app/pages/home.page.ts
+++ src/app/pages/home.page.ts
@@ -1,5 +1,12 @@
 import { Component, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
-@Component({ selector: 'lib-home-page', templateUrl: './home.page.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
+@Component({
+    selector: 'lib-home-page',
+    templateUrl: './home.page.html',
+    encapsulation: ViewEncapsulation.None,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
+})
 export class HomePage {
 }
