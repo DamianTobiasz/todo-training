e5e504e616d0c746d312b287f889c81b018d6809
Damian Tobiasz <damiant12345@gmail.com>
2022-03-10 14:44:05 +0100

    Added teamMemberDTO interface

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -3,7 +3,8 @@ import {
   ViewEncapsulation,
   ChangeDetectionStrategy,
 } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { TeamMemberDTO } from '../../../application/ports/secondary/team-member.dto';
 
 @Component({
   selector: 'lib-our-team',
@@ -12,18 +13,21 @@ import { of } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OurTeamComponent {
-  employees$ = of([
+  teamMembers$: Observable<TeamMemberDTO[]> = of([
     {
       name: 'Andrzej',
-      url: 'https://euro-kontakt.eu/wp-content/uploads/2020/04/pracownik-rolny.png.webp',
+      imageUrl:
+        'https://euro-kontakt.eu/wp-content/uploads/2020/04/pracownik-rolny.png.webp',
     },
     {
       name: 'Halina',
-      url: 'https://www.greentalent.pl/media/1187/dsce_3629b.jpg?anchor=center&mode=crop&width=730&height=530&rnd=132899353090000000',
+      imageUrl:
+        'https://www.greentalent.pl/media/1187/dsce_3629b.jpg?anchor=center&mode=crop&width=730&height=530&rnd=132899353090000000',
     },
     {
       name: 'Marek',
-      url: 'https://filplast.pl/wp-content/uploads/2021/03/pracownik-produkcji.jpg',
+      imageUrl:
+        'https://filplast.pl/wp-content/uploads/2021/03/pracownik-produkcji.jpg',
     },
   ]);
 }
--- src/app/pages/employee-details.page.ts
+++ src/app/pages/employee-details.page.ts
@@ -4,8 +4,6 @@ import {
   ChangeDetectionStrategy,
   OnInit,
 } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-employee-details-page',
@@ -13,7 +11,4 @@ import { Observable } from 'rxjs';
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EmployeeDetailsPage {
-  constructor(private activatedRoute: ActivatedRoute) {}
-  params$ = this.activatedRoute.params;
-}
+export class EmployeeDetailsPage {}
