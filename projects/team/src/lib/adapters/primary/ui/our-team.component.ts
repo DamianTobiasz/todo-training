558711ca7b2b7d167cd28921d58cf7f649f66020
Damian Tobiasz <damiant12345@gmail.com>
2022-03-11 12:44:13 +0100

    TeamMemberDTO as EmployeeDTO from 3E1

--- projects/team/src/lib/adapters/primary/ui/our-team.component.ts
+++ projects/team/src/lib/adapters/primary/ui/our-team.component.ts
@@ -15,19 +15,25 @@ import { TeamMemberDTO } from '../../../application/ports/secondary/team-member.
 export class OurTeamComponent {
   teamMembers$: Observable<TeamMemberDTO[]> = of([
     {
+      id: '1',
       name: 'Andrzej',
       imageUrl:
         'https://euro-kontakt.eu/wp-content/uploads/2020/04/pracownik-rolny.png.webp',
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
     },
     {
+      id: '2',
       name: 'Halina',
       imageUrl:
         'https://www.greentalent.pl/media/1187/dsce_3629b.jpg?anchor=center&mode=crop&width=730&height=530&rnd=132899353090000000',
+      bio: 'Tempor incididunt ut labore et dolore magna aliqua. Cras sed felis eget',
     },
     {
+      id: '3',
       name: 'Marek',
       imageUrl:
         'https://filplast.pl/wp-content/uploads/2021/03/pracownik-produkcji.jpg',
+      bio: 'velit. Nam at lectus urna duis convallis convallis tellus id. Eget aliquet',
     },
   ]);
 }
--- projects/team/src/lib/application/ports/secondary/team-member.dto.ts
+++ projects/team/src/lib/application/ports/secondary/team-member.dto.ts
@@ -1,4 +1,6 @@
 export interface TeamMemberDTO {
+  readonly id: string;
   readonly name: string;
   readonly imageUrl: string;
+  readonly bio: string;
 }
