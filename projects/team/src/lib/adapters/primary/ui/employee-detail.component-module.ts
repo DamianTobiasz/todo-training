05c676dd118cecc64fa139ca652427d385af38ab
Damian Tobiasz <damiant12345@gmail.com>
2022-03-17 16:47:53 +0100

    Visual upgrade of employee Detail component

--- projects/team/src/lib/adapters/primary/ui/employee-detail.component-module.ts
+++ projects/team/src/lib/adapters/primary/ui/employee-detail.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EmployeeDetailComponent } from './employee-detail.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [ModalModule.forRoot(), CommonModule],
   declarations: [EmployeeDetailComponent],
   providers: [],
   exports: [EmployeeDetailComponent],
--- projects/team/src/lib/adapters/primary/ui/employee-detail.component.ts
+++ projects/team/src/lib/adapters/primary/ui/employee-detail.component.ts
@@ -12,6 +12,9 @@ import {
   GetsOneTeamMemberDtoPort,
 } from '../../../application/ports/secondary/gets-one-team-member.dto-port';
 
+import { TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 @Component({
   selector: 'lib-employee-detail',
   templateUrl: './employee-detail.component.html',
@@ -22,10 +25,16 @@ export class EmployeeDetailComponent {
   teamMember$: Observable<TeamMemberDTO> = this._getsOneTeamMemberDto.getOne(
     this.activatedRoute.snapshot.params.employeeId
   );
+  modalRef?: BsModalRef;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     @Inject(GETS_ONE_TEAM_MEMBER_DTO)
-    private _getsOneTeamMemberDto: GetsOneTeamMemberDtoPort
+    private _getsOneTeamMemberDto: GetsOneTeamMemberDtoPort,
+    private modalService: BsModalService
   ) {}
+
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template);
+  }
 }
